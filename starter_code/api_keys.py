d4cc7c02dc099567b5d3a856c2603b1058e6877f
Scott Dowling <spdowling@gmail.com>
2020-01-28 14:18:57 -0500

    Code Update

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "4264edfd551f14b293faff374246cc9f"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAMWsR01P0f6kVGMrAXOo9IcCGMmQXUqXI"
